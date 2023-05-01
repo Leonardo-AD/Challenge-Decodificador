c31bdcc49a2758086c5fe85a47e2cd4b2c5d92f8
Leonardo Alves <leonardo.alves779@gmail.com>
2023-05-01 14:44:43 -0300

    creating a function to cryptograph button

---
+++ export.js
@@ -0,0 +1,6 @@
+// Exporting variables
+export const cryptographButton = document.querySelector(".cryptograph-button")
+export const decryptionButton = document.querySelector(".decryption-button")
+
+export const textField = document.querySelector("#text-field")
+
--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-// mudar cor do shorticon!
+// Importing variables
+
+import { cryptographButton, decryptionButton, textField } from "./export.js";
+
+cryptographButton.addEventListener("click", () => {
+    
+    if(!textField.value || textField.value == " "){
+        
+        Swal.fire({
+            title      : "Nenhuma mensagem para criptografar",
+            text       : "Por favor, digite alguma mensagem com letras minúsculas, sem acentos e sem caracteres especiais.",
+            icon       : "error",
+            background : "#F3F5FC"
+        })
+    }
+    else{
+        cryptograph()
+    }
+})
+
+function cryptograph(){
+    return console.log("Encrypted")
+
+    // let pattern = "/*-${}/"
+    // let re = new RegExp(pattern);
+    // console.log(re)
+}
